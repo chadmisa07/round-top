1a19b0042f3c0ab7bccf3687a119d06f35b39a21
sevslorillajr <severinolorillajr@gmail.com>
2023-12-04 02:30:24 +0800

    Stripe Setup

--- server/server.js
+++ server/server.js
@@ -9,10 +9,16 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = require('twilio')(accountSid, authToken);
 
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
 const app = express();
 app.use(cors());
 app.use(express.json());// receive form data 
 // app.use(express.urlencoded({extended: true, limit: '1mb'}))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const STRIPE_DOMAIN = `${process.env.DOMAIN}\bagels`;
 
 const db = mysql.createConnection({
     host     : process.env.DB_HOST,
@@ -45,6 +51,7 @@ app.get("/deliveryList", async (req, res) => {
     res.send({"message": 'Something went wrong, please consult with your provider!'});  
   }
 });
+
 app.post("/sendSMS", async(req, res) => {  
   try {
     const { phone_number, message } = req.body;
@@ -59,6 +66,114 @@ app.post("/sendSMS", async(req, res) => {
     console.log(err);
   }
 });
+
+app.post('/create-checkout-session', async (req, res) => {
+  const prices = await stripe.prices.list({
+    lookup_keys: [req.body.lookup_key],
+    expand: ['data.product'],
+  });
+  const session = await stripe.checkout.sessions.create({
+    billing_address_collection: 'auto',
+    line_items: [
+      {
+        price: prices.data[0].id,
+        // For metered billing, do not pass quantity
+        quantity: 1,
+
+      },
+    ],
+    mode: 'subscription',
+    success_url: `${STRIPE_DOMAIN}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${STRIPE_DOMAIN}/cancel.html`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.post('/create-portal-session', async (req, res) => {
+  // For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
+  // Typically this is stored alongside the authenticated user in your database.
+  const { session_id } = req.body;
+  const checkoutSession = await stripe.checkout.sessions.retrieve(session_id);
+
+  // This is the url to which the customer will be redirected when they are done
+  // managing their billing with the portal.
+  const returnUrl = STRIPE_STRIPE_DOMAIN;
+
+  const portalSession = await stripe.billingPortal.sessions.create({
+    customer: checkoutSession.customer,
+    return_url: returnUrl,
+  });
+
+  res.redirect(303, portalSession.url);
+});
+
+app.post(
+  '/stripe-webhook',
+  express.raw({ type: 'application/json' }),
+  (request, response) => {
+    let event = request.body;
+    // Replace this endpoint secret with your endpoint's unique secret
+    // If you are testing with the CLI, find the secret by running 'stripe listen'
+    // If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+    // at https://dashboard.stripe.com/webhooks
+    const endpointSecret = process.env.STRIPE_SECRET;
+    // Only verify the event if you have an endpoint secret defined.
+    // Otherwise use the basic event deserialized with JSON.parse
+    if (endpointSecret) {
+      // Get the signature sent by Stripe
+      const signature = request.headers['stripe-signature'];
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+    }
+    let subscription;
+    let status;
+    // Handle the event
+    switch (event.type) {
+      case 'customer.subscription.trial_will_end':
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription trial ending.
+        // handleSubscriptionTrialEnding(subscription);
+        break;
+      case 'customer.subscription.deleted':
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription deleted.
+        // handleSubscriptionDeleted(subscriptionDeleted);
+        break;
+      case 'customer.subscription.created':
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription created.
+        // handleSubscriptionCreated(subscription);
+        break;
+      case 'customer.subscription.updated':
+        subscription = event.data.object;
+        status = subscription.status;
+        console.log(`Subscription status is ${status}.`);
+        // Then define and call a method to handle the subscription update.
+        // handleSubscriptionUpdated(subscription);
+        break;
+      default:
+        // Unexpected event type
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
 
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
